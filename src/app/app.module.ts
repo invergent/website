5e6d5f3b775d3e92bb55eeb7b8bab1f4501a3048
Invergent <invergenttechnologies@gmail.com>
2019-02-10 22:50:45 +0100

    feat(coming-soon): create coming soon page
    - add normalize for consistent rendering
    - configure routing
    - redirect from home to coming soon page
    - build out coming soon page and style
    
    [Deliver #163843916]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ComingSoonComponent } from './coming-soon/coming-soon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ComingSoonComponent
   ],
   imports: [
     BrowserModule,
